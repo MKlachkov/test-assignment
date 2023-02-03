77016f5139bcf420d9a2844e481fb3b7b148cf2e
MKlachkov <mikhail_klachkov@mail.ru>
2023-02-04 00:35:15 +0300

    Redesigned model.js

--- js/modal.js
+++ js/modal.js
@@ -1,41 +1,21 @@
 const login = document.querySelector('.button.login')
 const register = document.querySelector('.button.registration')
-const modaEnd = document.querySelector('.close__modal')
+const modalEnd = document.querySelector('.close__modal')
 let isShow = false;
+function showModal(){
+    const modal = document.querySelector('.modal')
+    if (isShow){
+        modal.style.removeProperty('display')
+        modal.style.display = ''
+        isShow = false
+    }else{
+        modal.style.removeProperty('display')
+        modal.style.display = 'flex'
+        isShow = true
+    }
+};
 
-register.onclick=function (){
-    const modal = document.querySelector('.modal')
-    if (isShow){
-        modal.style.removeProperty('display')
-        modal.style.display = ''
-        isShow = false
-    }else{
-        modal.style.removeProperty('display')
-        modal.style.display = 'flex'
-        isShow = true
-    }
-};
-login.onclick=function (){
-    const modal = document.querySelector('.modal')
-    if (isShow){
-        modal.style.removeProperty('display')
-        modal.style.display = ''
-        isShow = false
-    }else{
-        modal.style.removeProperty('display')
-        modal.style.display = 'flex'
-        isShow = true
-    }
-};
-modaEnd.onclick=function(){
-    const modal = document.querySelector('.modal')
-    if (isShow){
-        modal.style.removeProperty('display')
-        modal.style.display = ''
-        isShow = false
-    }else{
-        modal.style.removeProperty('display')
-        modal.style.display = 'flex'
-        isShow = true
-    }
-}
+
+register.onclick=()=>{showModal()};
+login.onclick=()=>{showModal()};
+modalEnd.onclick=()=>{showModal()};
