58fd2930d773fd0f3200563e2a247712c876f814
MKlachkov <mikhail_klachkov@mail.ru>
2023-02-04 03:11:19 +0300

    modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,21 +1,17 @@
 const login = document.querySelector('.button.login')
 const register = document.querySelector('.button.registration')
 const modalEnd = document.querySelector('.close__modal')
+const modal = document.querySelector('.modal')
 let isShow = false;
-function showModal(){
-    const modal = document.querySelector('.modal')
-    if (isShow){
-        modal.style.removeProperty('display')
-        modal.style.display = ''
-        isShow = false
-    }else{
+function showModal() {
         modal.style.removeProperty('display')
         modal.style.display = 'flex'
-        isShow = true
-    }
 };
 
-
-register.onclick=()=>{showModal()};
-login.onclick=()=>{showModal()};
-modalEnd.onclick=()=>{showModal()};
+function closeModel() {
+    modal.style.removeProperty('display')
+    modal.style.display = ''
+};
+register.onclick = () => { showModal() };
+login.onclick = () => { showModal() };
+modalEnd.onclick = () => { closeModel() };
