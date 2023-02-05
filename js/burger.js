60d31c42186d56c3a606d54da1ae69079aa9c8c0
MKlachkov <mikhail_klachkov@mail.ru>
2023-02-06 01:33:58 +0300

    Project without galp

--- js/burger.js
+++ js/burger.js
@@ -1,17 +1,15 @@
 const burger = document.querySelector('.burger')
-const Menu = document.querySelector('.burger__menu');
+const Menu = document.querySelector('.burger__menu')
 let isClosed = true;
-const height = window.screen.height;
 function show(){
     Menu.classList.toggle("visible")
     document.querySelector('.login-box').classList.toggle("hide")
     if(isClosed){
-        burger.src='./images/close.svg'
+        burger.src='./images/burger/close.svg'
         isClosed = false
     }else{
-        burger.src='./images/burger.svg'
+        burger.src='./images/burger/burger.svg'
         isClosed=true
-        document.querySelector("body").style.overflowY='auto'
     }
 }
 burger.onclick=function(){
