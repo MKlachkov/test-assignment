72bb68391e6af7e2c0c4bd91d6bd9075c8df2fe0
MKlachkov <mikhail_klachkov@mail.ru>
2023-02-06 19:26:57 +0300

    fix and updates

--- app/js/modal.js
+++ app/js/modal.js
@@ -2,16 +2,13 @@ const login = document.querySelector('.button.login')
 const register = document.querySelector('.button.registration')
 const modalEnd = document.querySelector('.close__modal')
 const modal = document.querySelector('.modal')
-var isShow = false;
-function showModal() {
-        modal.style.removeProperty('display')
-        modal.style.display = 'flex'
+function toggleVisibility(option) {
+    if (option) {
+        modal.classList.toggle('toggleModal')
+    } else {
+        modal.classList.toggle('toggleModal')
+    }
 };
-
-function closeModel() {
-    modal.style.removeProperty('display')
-    modal.style.display = ''
-};
-register.onclick = () => { showModal() };
-login.onclick = () => { showModal() };
-modalEnd.onclick = () => { closeModel() };
+register.onclick = () => { toggleVisibility(true) };
+login.onclick = () => { toggleVisibility(true) };
+modalEnd.onclick = () => { toggleVisibility(false) };
--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,10 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglifyjs'),
     pump = require('pump'),
-    minify = require('gulp-minify');
-browserSync = require('browser-sync')
+    minify = require('gulp-minify'),
+    browserSync = require('browser-sync')
 
-
+//минимизация html
 gulp.task('minify-html', async function () {
     return gulp.src('app/index.html')
         .pipe(htmlmin({
@@ -18,33 +18,25 @@ gulp.task('minify-html', async function () {
         }))
         .pipe(gulp.dest('dist'));
 });
-gulp.task('browser-sync', function () {
-    browserSync({
-        server: {
-            baseDir: 'dist/index.html'
-        },
-        notify: false
-    });
-});
 
-gulp.task('clean', function (done) {
-    del.sync('dist');
-    done();
-});
-
+// gulp.task('clean', function (done) {
+//     del.sync('dist');
+//     done();
+// });
+//Компиляция SASS
 gulp.task('sass', async function () {
     return gulp.src('app/sass/main.scss')
         .pipe(sass())
         .pipe(gulp.dest('app/css'))
 });
-
+//Минимизация CSS
 gulp.task('styles', async function () {
     return gulp.src('app/css/main.css')
         .pipe(cssnano())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('dist/css'));
 });
-
+//Объединение скриптов
 gulp.task('scripts', function () {
     return gulp.src([
         "app/js/burger.js",
@@ -54,11 +46,21 @@ gulp.task('scripts', function () {
         .pipe(concat('libs.js'))
         .pipe(gulp.dest('app/js'));
 });
-
+//Сжатие скриптов
 gulp.task('compress', async function () {
     gulp.src(['app/js/libs.js'])
         .pipe(minify())
         .pipe(gulp.dest('dist/js'))
 });
+//Запуск приложения
+gulp.task('run', async function () {
+    browserSync.init({
+        server: {
+            baseDir: "dist"
+        },
+        notify: false
+    });
+});
+
 
 gulp.task('build', gulp.series('sass', 'styles', 'scripts', 'compress', 'minify-html'));
